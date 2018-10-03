9d8f6215812c08aeb70329d0156d48520161708e
Andrew McDermott <aim@frobware.com>
2018-10-03 20:14:37 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='xdwim',
+    version='0.1',
+    scripts=['xdwim', 'xdwimctl'],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
