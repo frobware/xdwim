a94a78dc849bdc675bd70160c8fb37d7309cc576
Andrew McDermott <aim@frobware.com>
2019-02-14 21:25:28 +0000

    Checkpoint

--- src/bin/rxdwimctl.rs
+++ src/bin/rxdwimctl.rs
@@ -2,13 +2,16 @@ use std::env;
 use std::os::unix::net::UnixStream;
 use std::path::Path;
 use std::io::Write;
-//use std::io::Read;
+use std::io::BufReader;
+use std::io::BufRead;
+
+// https://docs.w3cub.com/rust/std/os/unix/net/struct.unixlistener/
 
 extern crate xdwim;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
+    let mut args: Vec<String> = env::args().collect();
+    
     if args.len() <= 1 {
         println!("insufficient arguments");
         return
@@ -24,14 +27,38 @@ fn main() {
         Ok(stream) => stream,
     };
 
+    args.push("\n".to_string());
+        
     let joined = &args[1..].join(" ");
 
-    match stream.write(joined.as_bytes ()) {
+    match stream.write(joined.as_bytes()) {
         Err(err) => panic!("couldn't send message: {}", err),
         Ok(_) => {},
     }
 
-    let mut buffer = vec![0; 10];
-    stream.read_to_end(&mut buffer).unwrap();
-    println!("{:?}", buffer.as_slice());
+    // match stream.write(b'\n'.as_bytes()) {
+    //     Err(err) => panic!("couldn't send message: {}", err),
+    //     Ok(_) => {},
+    // }
+
+    // let mut buf = vec![];
+    let mut reader = BufReader::new(stream);
+    let mut line = String::new();
+    let len = reader.read_line(&mut line);
+    println!("First line is {:?} bytes long", len);
+
+// match reader.read_line(&mut line) {
+//     Err(err) => panic!("couldn't read message: {}", err),
+//     Ok(len) => {
+//         println!("First line is {} bytes long", len);
+//     },
+// }
+
+//println!("First line is {} bytes long", len);
+
+// match rdr.read_line(&mut buf) {
+//     Err(err) => panic!("couldn't read message: {}", err),
+//     Ok(0) => {},
+//     Ok(_) => {},
+// }
 }
