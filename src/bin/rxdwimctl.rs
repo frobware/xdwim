17962bb4c2aea950e03500182a46d4ebefb63f26
Andrew McDermott <aim@frobware.com>
2019-02-24 19:19:28 +0000

    rust: working version of xdwimctl
    Could replace the Python version now.

--- src/bin/rxdwimctl.rs
+++ src/bin/rxdwimctl.rs
@@ -4,17 +4,16 @@ use std::path::Path;
 use std::io::Write;
 use std::io::BufReader;
 use std::io::BufRead;
-
-// https://docs.w3cub.com/rust/std/os/unix/net/struct.unixlistener/
+use std::process::Command;
 
 extern crate xdwim;
 
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
-    
+    let args: Vec<String> = env::args().collect();
+
     if args.len() <= 1 {
-        println!("insufficient arguments");
-        return
+        println!("insufficient arguments!");
+        return;
     }
 
     let socket = Path::new(xdwim::SOCKET_PATH);
@@ -27,38 +26,27 @@ fn main() {
         Ok(stream) => stream,
     };
 
-    args.push("\n".to_string());
-        
     let joined = &args[1..].join(" ");
 
     match stream.write(joined.as_bytes()) {
         Err(err) => panic!("couldn't send message: {}", err),
-        Ok(_) => {},
+        Ok(_) => {}
     }
 
-    // match stream.write(b'\n'.as_bytes()) {
-    //     Err(err) => panic!("couldn't send message: {}", err),
-    //     Ok(_) => {},
-    // }
+    match stream.write("\n".as_bytes()) {
+        Err(err) => panic!("couldn't send message: {}", err),
+        Ok(_) => {}
+    }
 
-    // let mut buf = vec![];
     let mut reader = BufReader::new(stream);
     let mut line = String::new();
-    let len = reader.read_line(&mut line);
-    println!("First line is {:?} bytes long", len);
 
-// match reader.read_line(&mut line) {
-//     Err(err) => panic!("couldn't read message: {}", err),
-//     Ok(len) => {
-//         println!("First line is {} bytes long", len);
-//     },
-// }
-
-//println!("First line is {} bytes long", len);
-
-// match rdr.read_line(&mut buf) {
-//     Err(err) => panic!("couldn't read message: {}", err),
-//     Ok(0) => {},
-//     Ok(_) => {},
-// }
+    match reader.read_line(&mut line) {
+        Err(err) => panic!("couldn't read message: {}", err),
+        Ok(_) => {
+            if !line.starts_with("success") {
+                Command::new(&args[2]).args(&args[3..]).spawn();
+            }
+        }
+    }
 }
