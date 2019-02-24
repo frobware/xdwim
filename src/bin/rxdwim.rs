46d50311499349f28f099bb4546ac0b475e0a447
Andrew McDermott <aim@frobware.com>
2019-02-24 20:50:58 +0000

    rust: rxdwim: checkpoint

--- src/bin/rxdwim.rs
+++ src/bin/rxdwim.rs
@@ -1,28 +1,56 @@
-use std::thread;
-use std::os::unix::net::{UnixStream, UnixListener};
-
 extern crate xdwim;
+extern crate libxdo_sys;
 
-fn handle_client(stream: UnixStream) {
-    println!("handle_clientr: {:?}", stream);
+use std::io::BufRead;
+use std::io::BufReader;
+use std::os::unix::net::UnixStream;
+use std::os::unix::net::UnixListener;
+use std::ptr;
+
+use libxdo_sys::xdo_free;
+use libxdo_sys::xdo_new;
+use libxdo_sys::xdo_t;
+
+fn handle_client(_xdo: *mut xdo_t, stream: UnixStream) {
+    let mut reader = BufReader::new(stream);
+    let mut line = String::new();
+
+    match reader.read_line(&mut line) {
+        Err(err) => println!("couldn't read message: {}", err),
+        Ok(_) => {
+            println!("You said: {}", line);
+        }
+    }
 }
 
-fn main() {
-    let listener = UnixListener::bind(xdwim::SOCKET_PATH).unwrap();
+fn main() -> Result<(), Box<std::error::Error>> {
+    let xdo;
 
-    // accept connections, spawning a new thread on each connection.
+    unsafe {
+        xdo = xdo_new(ptr::null());
+
+        if xdo.is_null() {
+            panic!("Failed to initialise libxdo.");
+        }
+    }
+
+    let listener = UnixListener::bind(xdwim::SOCKET_PATH)?;
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
-                // connection succeeded
-                thread::spawn(|| handle_client(stream));
-            }
-
             Err(err) => {
                 println!("accept function failed: {:?}", err);
                 break;
             }
+            Ok(stream) => {
+                handle_client(xdo, stream);
+            }
         }
     }
+
+    unsafe {
+        xdo_free(xdo);
+    }
+
+    Ok(())
 }
