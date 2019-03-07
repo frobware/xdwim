a21321ca17dcde185e277f9037bf099cb0149865
Andrew McDermott <aim@frobware.com>
2019-03-07 18:08:20 +0000

    Kill the snake
    Delete the Python implementation.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='xdwim',
-    version='0.1',
-    scripts=['xdwim', 'xdwimctl'],
-    license='MIT',
-    long_description=open('README.md').read(),
-)
